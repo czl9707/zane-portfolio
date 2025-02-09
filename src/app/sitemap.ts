b7e1b8b20010196b97ca9a4c5114a87688c51e9e
zane <czl970721@gmail.com>
2025-02-09 14:42:18 -0500

    append root to sitemap

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,12 +1,14 @@
 import type { MetadataRoute } from 'next'
 import * as ZaneArchProjects from '@/lib/cms/zane-arch-project'
 
+const ROOT = "https://zane-portfolio.kiyo-n-zane.com/";
+
 const singlePages = [
-    'https://zane-portfolio.kiyo-n-zane.com/',
-    'https://zane-portfolio.kiyo-n-zane.com/about',
-    'https://zane-portfolio.kiyo-n-zane.com/as/architect/project',
-    'https://zane-portfolio.kiyo-n-zane.com/as/developer/project',
-    'https://zane-portfolio.kiyo-n-zane.com/as/developer/blog',
+    `${ROOT}`,
+    `${ROOT}about`,
+    `${ROOT}as/architect/project`,
+    `${ROOT}as/developer/project`,
+    `${ROOT}as/developer/blog`,
 ]
 
 export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
@@ -20,7 +22,7 @@ export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
             priority: 1,
         })) as MetadataRoute.Sitemap),
         ...(archProjects.map((p) => ({
-            url: `/as/architect/project/${p.title.replaceAll(" ", "_")}`,
+            url: `${ROOT}as/architect/project/${p.title.replaceAll(" ", "_")}`,
             lastModified: new Date(),
             changeFrequency: 'monthly',
         })) as MetadataRoute.Sitemap)
