43404f26d8cece2049729eea4db6c28fcb0af6e8
zane <czl970721@gmail.com>
2025-04-22 21:12:25 -0400

    fix header

--- src/components/layout/sticky-hero.tsx
+++ src/components/layout/sticky-hero.tsx
@@ -11,13 +11,28 @@ import heroStyle from './sticky-hero.module.css'
 const StickyHero = React.forwardRef<HTMLDivElement, Slot.SlotProps & { asChild?: boolean }>(
     function StickyHero({ className, asChild = false, children, style, ...others }, ref) {
         const Comp = asChild ? Slot.Root : 'div';
+        const [height, setHeight] = React.useState<string>("100vh")
         const localRef = React.useRef<HTMLDivElement>(undefined);
+
+        React.useEffect(() => {
+            const ch = localRef.current?.clientHeight;
+            setHeight(ch == undefined ? "100vh" : `${ch}px`);
+
+            if (localRef.current) {
+                const tgt = localRef.current;
+                const observer = new ResizeObserver(() => {
+                    setHeight(`${tgt.clientHeight}px`);
+                });
+                observer.observe(tgt);
+                return () => observer.unobserve(tgt);
+            }
+        }, [localRef])
 
         return (
             <Comp className={clsx(heroStyle.StickyHero, className)}
                 style={{
                     ...style,
-                    "--size-hero-height": `${localRef.current?.clientHeight}px`
+                    "--size-hero-height": height
                 } as React.CSSProperties}
                 {...others} ref={forkRefs(localRef, ref)}>
                 {children}
