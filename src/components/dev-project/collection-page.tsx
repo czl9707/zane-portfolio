d920405fced0aa16b7f790ea7939b347708b28b3
zane <czl970721@gmail.com>
2025-08-31 14:21:37 -0400

    smaller fonts for blog title

--- src/components/dev-project/collection-page.tsx
+++ src/components/dev-project/collection-page.tsx
@@ -5,7 +5,6 @@ import * as T from "@/components/ui/typography";
 import * as ZaneDevProject from '@/lib/cms/zane-dev-project'
 import ContentSection from '@/components/layout/content-section';
 import DevProjectBrief from '@/components/dev-project/brief';
-import BriefsContainer from '@/components/layout/briefs-container'
 
 export async function Page() {
     const projects = (await ZaneDevProject.getAll());
@@ -13,15 +12,15 @@ export async function Page() {
     return (
         <ContentSection
             style={{ paddingTop: 0 }}
-            header={<T.H5 asElement='h2' style={{ opacity: 0.75 }}>All Projects</T.H5>}
+            header={<T.H6 asElement='h2' style={{ opacity: 0.75 }}>All Projects</T.H6>}
         >
-            <BriefsContainer style={{ gridColumn: "span 3 / span 3", }}>
+            <DevProjectBrief.Container style={{ gridColumn: "span 3 / span 3", }}>
                 {
                     projects.map((project) => (
-                        <DevProjectBrief project={project} key={project.title} />
+                        <DevProjectBrief withDescription project={project} key={project.title} />
                     ))
                 }
-            </BriefsContainer>
+            </DevProjectBrief.Container>
         </ContentSection>
     )
 }
