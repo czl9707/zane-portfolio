7263aefab35092896c4a5001ff46b1e7d9edbf47
zane <czl970721@gmail.com>
2025-09-01 13:44:31 -0400

    add sitemap

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,6 +1,7 @@
 import type { MetadataRoute } from 'next'
 import * as ZaneArchProjects from '@/lib/cms/zane-arch-project'
 import * as ZaneBlog from '@/lib/cms/zane-blog'
+import * as ZaneNote from '@/lib/cms/zane-note'
 
 const ROOT = "https://zane-portfolio.kiyo-n-zane.com/";
 
@@ -16,6 +17,7 @@ export const revalidate = 14400;
 export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
     const archProjects = await ZaneArchProjects.getAll();
     const blogs = await ZaneBlog.getAll();
+    const notes = await ZaneNote.getAll();
 
     return [
         ...(singlePages.map(p => ({
@@ -24,14 +26,16 @@ export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
             changeFrequency: 'weekly',
             priority: 1,
         })) as MetadataRoute.Sitemap),
-        ...(archProjects.map((p) => ({
-            url: `${ROOT}project/by/architect/${p.id}`,
-            lastModified: new Date(),
+        ...(archProjects.map((project) => ({
+            url: `${ROOT}project/by/architect/${project.id}`,
             changeFrequency: 'monthly',
         })) as MetadataRoute.Sitemap),
-        ...(blogs.map((p) => ({
-            url: `${ROOT}blog/by/developer/${p.id}`,
-            lastModified: new Date(),
+        ...(blogs.map((blog) => ({
+            url: `${ROOT}blog/by/${blog.role}/${blog.id}`,
+            changeFrequency: 'monthly',
+        })) as MetadataRoute.Sitemap),
+        ...(notes.map((note) => ({
+            url: `${ROOT}blog/by/${note.role}/${note.id}`,
             changeFrequency: 'monthly',
         })) as MetadataRoute.Sitemap)
     ]
