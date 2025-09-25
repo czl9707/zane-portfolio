8b2fa083feb30581e29583f98ce0247dafcc8b9c
zane <czl970721@gmail.com>
2025-09-24 21:15:21 -0400

    add vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -12,43 +12,52 @@ import { RehypeReferenceTranslation } from './src/lib/markdown/rehype-reference-
 import { rehypeHashStyleHeadings } from './src/lib/markdown/rehype-hash-style-headings';
 import { rehypeSectionize } from './src/lib/markdown/rehype-sectionize';
 
+import vercel from "@astrojs/vercel";
+
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://zane-portfolio.kiyo-n-zane.com',
-    integrations: [
-        sitemap({
-            filter(path){return !path.includes("/as/")}
-        }), 
-        react()
-    ],
-    trailingSlash: "ignore",
-    compressHTML: true,
-    env: {
-        schema: {
-            ADMIN_URL: envField.number({ context: "server", access: "secret" }),
-            ADMIN_APIKEY: envField.string({ context: "server", access: "secret" }),
-        }
-    },
-    markdown: {
-        syntaxHighlight: "shiki",
-        shikiConfig: {
-            theme: 'github-dark-high-contrast',
-        },
-        remarkPlugins:[
-            remarkMath,
-            remarkGfm,
-        ],
-        rehypePlugins:[
-            RehypeReferenceTranslation,
-            rehypeHeadingIds,
-            rehypeHashStyleHeadings,
-            rehypeSectionize,
-            rehypeMathjax
-        ],
-    },
-    redirects: {
-        "/as/developer/blog": "/blog",
-        "/as/architect/project": "/project/by/architect",
-        "/as/developer/project": "/project/by/developer",
-    },
+  site: 'https://zane-portfolio.kiyo-n-zane.com',
+
+  integrations: [
+      sitemap({
+          filter(path){return !path.includes("/as/")}
+      }), 
+      react()
+  ],
+
+  trailingSlash: "ignore",
+  compressHTML: true,
+
+  env: {
+      schema: {
+          ADMIN_URL: envField.number({ context: "server", access: "secret" }),
+          ADMIN_APIKEY: envField.string({ context: "server", access: "secret" }),
+      }
+  },
+
+  markdown: {
+      syntaxHighlight: "shiki",
+      shikiConfig: {
+          theme: 'github-dark-high-contrast',
+      },
+      remarkPlugins:[
+          remarkMath,
+          remarkGfm,
+      ],
+      rehypePlugins:[
+          RehypeReferenceTranslation,
+          rehypeHeadingIds,
+          rehypeHashStyleHeadings,
+          rehypeSectionize,
+          rehypeMathjax
+      ],
+  },
+
+  redirects: {
+      "/as/developer/blog": "/blog",
+      "/as/architect/project": "/project/by/architect",
+      "/as/developer/project": "/project/by/developer",
+  },
+
+  adapter: vercel(),
 });
